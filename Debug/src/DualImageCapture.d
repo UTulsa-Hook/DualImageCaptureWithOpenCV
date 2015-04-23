73876edffd3a75e797407ffc9ab803ae7f2ded7f
hutch-hutchins <nathan-hutchins@utulsa.edu>
2015-04-23 08:30:15 -0500

    First Commit

--- Debug/src/DualImageCapture.d
+++ Debug/src/DualImageCapture.d
@@ -1,16 +1,16 @@
 src/DualImageCapture.d: ../src/DualImageCapture.cpp \
- /home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvIMPACT_CPP/mvIMPACT_acquire.h \
- /home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvDeviceManager/Include/mvDeviceManager.h \
- /home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/DriverBase/Include/mvDriverBaseEnums.h \
- /home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/DriverBase/Include/mvVersionInfo.h \
- /home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvPropHandling/Include/mvPropHandlingDatatypes.h
+ /home/hutch/workspace/DualImageCaptureWithOpenCV/mvIMPACT_CPP/mvIMPACT_acquire.h \
+ /home/hutch/workspace/DualImageCaptureWithOpenCV/mvDeviceManager/Include/mvDeviceManager.h \
+ /home/hutch/workspace/DualImageCaptureWithOpenCV/DriverBase/Include/mvDriverBaseEnums.h \
+ /home/hutch/workspace/DualImageCaptureWithOpenCV/DriverBase/Include/mvVersionInfo.h \
+ /home/hutch/workspace/DualImageCaptureWithOpenCV/mvPropHandling/Include/mvPropHandlingDatatypes.h
 
-/home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvIMPACT_CPP/mvIMPACT_acquire.h:
+/home/hutch/workspace/DualImageCaptureWithOpenCV/mvIMPACT_CPP/mvIMPACT_acquire.h:
 
-/home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvDeviceManager/Include/mvDeviceManager.h:
+/home/hutch/workspace/DualImageCaptureWithOpenCV/mvDeviceManager/Include/mvDeviceManager.h:
 
-/home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/DriverBase/Include/mvDriverBaseEnums.h:
+/home/hutch/workspace/DualImageCaptureWithOpenCV/DriverBase/Include/mvDriverBaseEnums.h:
 
-/home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/DriverBase/Include/mvVersionInfo.h:
+/home/hutch/workspace/DualImageCaptureWithOpenCV/DriverBase/Include/mvVersionInfo.h:
 
-/home/loyd-hook/0Projects/SV/software/eclipse_ws/DualImageCaptureWithOpenCV/mvPropHandling/Include/mvPropHandlingDatatypes.h:
+/home/hutch/workspace/DualImageCaptureWithOpenCV/mvPropHandling/Include/mvPropHandlingDatatypes.h:
